bd595300a77c778e4553e1aeed513fc60cf7307a
drvr4682 <drvr4682@gmail.com>
2026-03-29 22:42:42 +0530

    test: configure H2 database for test isolation

--- projectservice/src/test/java/com/pms/projectservice/ProjectControllerTest.java
+++ projectservice/src/test/java/com/pms/projectservice/ProjectControllerTest.java
@@ -8,12 +8,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 class ProjectControllerTest {
 
     @Autowired
