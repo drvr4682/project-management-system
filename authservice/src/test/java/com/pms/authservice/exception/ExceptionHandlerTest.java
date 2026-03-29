99bbe1ba7084ec3f964fcdac23af0447576c2f82
drvr4682 <drvr4682@gmail.com>
2026-03-29 17:44:18 +0530

    test: add context load test for global exception handler

---
+++ authservice/src/test/java/com/pms/authservice/exception/ExceptionHandlerTest.java
@@ -0,0 +1,13 @@
+package com.pms.authservice.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlerTest {
+
+    @Test
+    void contextLoads() {
+        // verifies application context loads with exception handler
+    }
+}
