7afc3a1579526ee1342e6b0d21d0af4e7f8b835c
drvr4682 <drvr4682@gmail.com>
2026-04-04 11:36:11 +0530

    fix: remove duplicate imports in GlobalExceptionHandler

--- projectservice/src/main/java/com/pms/projectservice/exception/GlobalExceptionHandler.java
+++ projectservice/src/main/java/com/pms/projectservice/exception/GlobalExceptionHandler.java
@@ -1,14 +1,7 @@
 package com.pms.projectservice.exception;
 
 import java.util.HashMap;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
